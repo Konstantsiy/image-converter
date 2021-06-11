633e168903bbd8591a8be4035c616a78e01dc323
idi_obnimu <constintinforgit@gmail.com>
2021-06-11 12:38:15 +0300

    Fix db.sql: change id type (serual -> uuid), add default values

--- scripts/db.sql
+++ scripts/db.sql
@@ -1,15 +1,10 @@
+create extension if not exists "uuid-ossp";
 create schema if not exists converter;
-
-create table if not exists converter.users (
-    id serial primary key,
-    email varchar(50) unique not null,
-    password varchar(50) not null
-);
 
 do $$
     begin
         if not exists(select 1 from pg_type where typname = 'file_format') then
-            create type file_format as enum ('jpg', 'png');
+            create type file_format as enum ('jpeg', 'png');
         end if;
     end;
 $$;
@@ -22,23 +17,29 @@ do $$
     end;
 $$;
 
+create table if not exists converter.users (
+    id uuid default uuid_generate_v1() primary key,
+    email varchar(50) unique not null,
+    password varchar(50) not null
+);
+
 create table if not exists converter.images (
-    id serial primary key,
+    id uuid default uuid_generate_v1() primary key,
     name varchar(80) not null,
     format file_format not null,
     location text not null
 );
 
 create table if not exists converter.requests (
-    id serial primary key,
-    user_id int not null,
-    image_id int not null,
+    id uuid default uuid_generate_v1() primary key,
+    user_id uuid not null,
+    image_id uuid not null,
     source_format file_format not null,
     target_format file_format not null,
-    ratio int not null,
+    ratio int check ( ratio > 0  and ratio < 100) not null,
     status status not null,
-    created timestamp default now() not null,
-    updated timestamp default now() not null,
+    created timestamp without time zone default current_timestamp not null,
+    updated timestamp without time zone default current_timestamp not null,
 
     foreign key (user_id) references converter.users(id),
     foreign key (image_id) references converter.requests(id)
