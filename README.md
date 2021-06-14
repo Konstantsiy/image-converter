687ccb2ad5a3acc30ec16550c1e9902b3128f144
const_int_in <62259384+Konstantsiy@users.noreply.github.com>
2021-06-14 12:42:50 +0300

    Deploy database in docker compose (#3)
    * Add README.md
    
    * Change README.md
    
    * Add db script, change README.md
    
    * Change architecture and database diagrams
    
    * Fix db.sql: add default values and additional fields
    
    * Fix README.md
    
    * Fix db.sql: change id type (serual -> uuid), add default values
    
    * Fix db.sql: add uuid extension if not exists
    
    * Fix db.sql: create db if not exists
    
    * Fix db.sql: add target_id to requests table
    
    * Fix db.sql: change foreign key
    
    * Change db diagram
    
    * Rename db.sql -> create_db.sql
    
    * Add docker-compose.yml
    
    * Change docker-compose.yml: delete ports
    
    * Delete .env file
    
    * Add Dockerfile and docker_db.sql script for create database
    
    * Change Dockerfile
    
    * Fix docker-compose.yml: add volumers

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# photo-converter
-Service that expose a RESTful API to convert JPEG to PNG and vice versa and compress the image with the compression ratio specified by the user.
+# image-converter
+Service that expose a RESTful API to convert JPEG to PNG and vice versa and compress the image 
+with the compression ratio specified by the user. The user has the ability to view
+the history and status of their requests (for example, queued, processed, completed) and upload 
+the original image and the processed one.
+# Architecture Diagram
+![alt text](./docs/architecture-diagram.png)
+# Endpoints
+- /requests - view user requests history [GET]
+- /user/login - user authorization [POST]
+- /user/signup - user registration [POST]
+- /user/logout - log out of the user [GET]
+- /conversion - convert needed image [POST]
+- /photos/{photoId} - get needed image [GET]
+# Database Scheme
+![alt text](./docs/db.png)
---
+++ scripts/create_db.sql
@@ -0,0 +1,61 @@
+select 'create database ita'
+where not exists(select from pg_database where datname = 'ita');
+\gexec
+
+\c ita
+
+create schema if not exists converter;
+
+do $$
+    begin
+        if not exists(select 1 from pg_extension where extname = 'uuid-ossp') then
+            create extension "uuid-ossp";
+        end if;
+    end
+$$;
+
+do $$
+    begin
+        if not exists(select 1 from pg_type where typname = 'file_format') then
+            create type file_format as enum ('jpeg', 'png');
+        end if;
+    end
+$$;
+
+do $$
+    begin
+        if not exists(select 1 from pg_type where typname = 'status') then
+            create type status as enum ('queued', 'processed', 'failed', 'done');
+        end if;
+    end
+$$;
+
+create table if not exists converter.users (
+    id uuid default uuid_generate_v1() primary key,
+    email varchar(50) unique not null,
+    password varchar(50) not null
+);
+
+create table if not exists converter.images (
+    id uuid default uuid_generate_v1() primary key,
+    name varchar(80) not null,
+    format file_format not null,
+    location text not null
+);
+
+create table if not exists converter.requests (
+    id uuid default uuid_generate_v1() primary key,
+    user_id uuid not null,
+    source_id uuid not null,
+    target_id uuid not null,
+    source_format file_format not null,
+    target_format file_format not null,
+    ratio int check ( ratio > 0  and ratio < 100) not null,
+    status status not null,
+    created timestamp without time zone default current_timestamp not null,
+    updated timestamp without time zone default current_timestamp not null,
+
+    foreign key (user_id) references converter.users(id),
+    foreign key (source_id) references converter.images(id),
+    foreign key (target_id) references converter.images(id)
+);
