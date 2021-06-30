194afaa9b5455333bd6f0dc93f87bdf703b6b471
idi_obnimu <constintinforgit@gmail.com>
2021-06-30 14:22:15 +0300

    Create domain/entity.go to represent db objects

---
+++ internal/domain/entity.go
@@ -0,0 +1,32 @@
+// Package domain represents the main entities that are used in the application.
+package domain
+
+import "time"
+
+// ImageInfo represents an image in the database.
+type ImageInfo struct {
+	Name     string
+	Format   string
+	Location string
+}
+
+// UserInfo represents the user in the database.
+type UserInfo struct {
+	ID       string
+	Email    string
+	Password string
+}
+
+// ConversionRequestInfo represents conversion request in the database.
+type ConversionRequestInfo struct {
+	ID           string
+	Name         string
+	SourceID     string
+	TargetID     string
+	SourceFormat string
+	TargetFormat string
+	Ratio        int
+	Created      time.Time
+	Updated      time.Time
+	Status       string
+}
