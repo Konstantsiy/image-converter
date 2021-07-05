117b27f42d047d6fa1251ff52cf3cea3d9b81683
idi_obnimu <constintinforgit@gmail.com>
2021-07-05 16:01:28 +0300

    Add JWT authorization

--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -1,11 +1,24 @@
 // Package auth provides the logic for working with JWT tokens.
 package auth
 
+import (
+	"fmt"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+const (
+	ACTimeout = 12 * time.Hour
+	RTTimeout = 48 * time.Hour
+)
+
 // TokenManager implements functionality for Access & Refresh tokens generation.
 type TokenManager struct {
 	privateKey string
 }
 
+//NewTokenManager returns new token manager with the given private key.
 func NewTokenManager(privateKey string) *TokenManager {
 	return &TokenManager{
 		privateKey: privateKey,
@@ -14,10 +27,42 @@ func NewTokenManager(privateKey string) *TokenManager {
 
 // GenerateAccessToken generates new access token.
 func (tm *TokenManager) GenerateAccessToken(userID string) (string, error) {
-	return "", nil
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		Subject:   userID,
+		ExpiresAt: time.Now().Add(ACTimeout).Unix(),
+		IssuedAt:  time.Now().Unix(),
+	})
+
+	return token.SignedString(tm.privateKey)
 }
 
 // GenerateRefreshToken generates new refresh token.
 func (tm *TokenManager) GenerateRefreshToken() (string, error) {
-	return "", nil
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		ExpiresAt: time.Now().Add(RTTimeout).Unix(),
+		IssuedAt:  time.Now().Unix(),
+	})
+
+	return token.SignedString(tm.privateKey)
+}
+
+//ParseToken parses the given token and returns the user ID.
+func (tm *TokenManager) ParseToken(accessToken string) (string, error) {
+	token, err := jwt.Parse(accessToken, func(token *jwt.Token) (i interface{}, err error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(tm.privateKey), nil
+	})
+	if err != nil {
+		return "", err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return "", fmt.Errorf("error get user claims from token")
+	}
+
+	return claims["sub"].(string), nil
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -2,6 +2,7 @@
 package server
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -13,6 +14,13 @@ import (
 	"github.com/Konstantsiy/image-converter/internal/repository"
 	"github.com/Konstantsiy/image-converter/internal/validation"
 	"github.com/gorilla/mux"
+)
+
+const (
+	AuthorizationHeader  = "Authorization"
+	NeededSecurityScheme = "Bearer"
+
+	userIDKey = iota
 )
 
 // Server represents application server.
@@ -32,8 +40,33 @@ func NewServer(repo *repository.Repository, tokenManager *auth.TokenManager) *Se
 // AuthMiddleware checks user authorization.
 func (s *Server) AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// check JWT tokens
-		next.ServeHTTP(w, r)
+		authHeader := r.Header.Get(AuthorizationHeader)
+		if authHeader == "" {
+			http.Error(w, "empty auth handler", http.StatusUnauthorized)
+			return
+		}
+
+		authHeaderParts := strings.Split(authHeader, " ")
+		if len(authHeaderParts) != 2 || authHeaderParts[0] != NeededSecurityScheme {
+			http.Error(w, "invalid auth header", http.StatusUnauthorized)
+			return
+		}
+
+		if len(authHeaderParts[1]) == 0 {
+			http.Error(w, "token is empty", http.StatusUnauthorized)
+			return
+		}
+
+		token := authHeaderParts[1]
+		claimsUserID, err := s.tokenManager.ParseToken(token)
+		if err != nil {
+			http.Error(w, "can't parse JWT", http.StatusUnauthorized)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), userIDKey, claimsUserID)
+
+		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
 
