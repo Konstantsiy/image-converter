cc24480ba5adb21a01a67cfca727f769b1ba206a
const_int_in <62259384+Konstantsiy@users.noreply.github.com>
2021-07-26 15:36:37 +0300

    Update config_test.go

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -7,7 +7,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// TestLoad tests Load function.
 func TestLoad(t *testing.T) {
 	require := require.New(t)
 
@@ -18,18 +17,20 @@ func TestLoad(t *testing.T) {
 	os.Setenv("DB_PORT", "5432")
 	os.Setenv("DB_SSL_MODE", "disable")
 	os.Setenv("JWT_PUBLIC_KEY", "123456789")
+	os.Setenv("JWT_PRIVATE_KEY", "1234567")
 
 	actual, err := Load()
 	require.NoError(err)
 
 	expected := Config{
-		Username:  "postgres",
-		Password:  "qwerty123",
-		DBName:    "ita",
-		Host:      "8080",
-		Port:      "5432",
-		SSLMode:   "disable",
-		PublicKey: "123456789",
+		Username:   "postgres",
+		Password:   "qwerty123",
+		DBName:     "ita",
+		Host:       "8080",
+		Port:       "5432",
+		SSLMode:    "disable",
+		PublicKey:  "123456789",
+		PrivateKey: "1234567",
 	}
 	require.Equal(expected, actual)
 }
