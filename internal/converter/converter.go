c9047bc1ab2f1ab9ce067882dc4dc5f61f8169bc
idi_obnimu <constintinforgit@gmail.com>
2021-08-04 14:47:59 +0300

    Fix converter.go: replace a file with a buffer

--- internal/converter/converter.go
+++ internal/converter/converter.go
@@ -2,14 +2,13 @@
 package converter
 
 import (
-	"errors"
+	"bytes"
 	"fmt"
 	"image"
 	"image/jpeg"
 	"image/png"
+	"io"
 	"mime/multipart"
-	"os"
-	"time"
 )
 
 const (
@@ -27,29 +26,26 @@ func NewConverter() *Converter {
 	return &Converter{}
 }
 
-//Convert converts and compresses the given image file according to the target format and compression ratio.
-func (c *Converter) Convert(file multipart.File, targetFormat string, ratio int) (*os.File, error) {
+// Convert converts and compresses the given image file according to the target format and compression ratio.
+func (c *Converter) Convert(file multipart.File, targetFormat string, ratio int) (io.ReadSeeker, error) {
 	imageData, _, err := image.Decode(file)
 	if err != nil {
 		return nil, fmt.Errorf("can't decode source file: %w", err)
 	}
 
-	outputFile, err := os.Create(TempPath + time.Now().String() + "." + targetFormat)
-	if err != nil {
-		return nil, errors.New("can't create target file")
-	}
+	buf := new(bytes.Buffer)
 
 	switch targetFormat {
 	case FormatPNG:
 		var enc png.Encoder
 		enc.CompressionLevel = png.CompressionLevel(ratio)
-		err := enc.Encode(outputFile, imageData)
+		err := enc.Encode(buf, imageData)
 		if err != nil {
 			return nil, fmt.Errorf("can't convert image to %s format: %w", FormatJPG, err)
 		}
 	case FormatJPEG, FormatJPG:
 		{
-			err := jpeg.Encode(outputFile, imageData, &jpeg.Options{
+			err := jpeg.Encode(buf, imageData, &jpeg.Options{
 				Quality: ratio,
 			})
 			if err != nil {
@@ -57,5 +53,6 @@ func (c *Converter) Convert(file multipart.File, targetFormat string, ratio int)
 			}
 		}
 	}
-	return outputFile, nil
+
+	return bytes.NewReader(buf.Bytes()), nil
 }
