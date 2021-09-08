809ea3395951914c1bdd6cdf464e9467456889be
idi_obnimu <constintinforgit@gmail.com>
2021-09-08 14:44:27 +0300

    Fix app.go

--- internal/app/app.go
+++ internal/app/app.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/Konstantsiy/image-converter/internal/queue"
+
 	"github.com/Konstantsiy/image-converter/internal/config"
 	"github.com/Konstantsiy/image-converter/internal/repository"
 	"github.com/Konstantsiy/image-converter/internal/server"
@@ -41,7 +43,22 @@ func Start() error {
 		return fmt.Errorf("storage error: %v", err)
 	}
 
-	s := server.NewServer(repo, tokenManager, st)
+	producer, err := queue.NewProducer(&conf)
+	if err != nil {
+		return fmt.Errorf("can't create publisher: %w", err)
+	}
+
+	consumer, err := queue.NewConsumer(repo, st, &conf)
+	if err != nil {
+		return fmt.Errorf("can't create consumer: %w", err)
+	}
+
+	err = consumer.ListenToQueue()
+	if err != nil {
+		return fmt.Errorf("queue consumer error: %w", err)
+	}
+
+	s := server.NewServer(repo, tokenManager, st, producer)
 	s.RegisterRoutes(r)
-	return http.ListenAndServe(":"+conf.Port, r)
+	return http.ListenAndServe(":8080", r)
 }
