1112c30aedf9754d696cb8a997641b5f2dfa8e80
idi_obnimu <constintinforgit@gmail.com>
2021-09-20 10:51:29 +0300

    Update config.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,6 +5,7 @@ import (
 	"github.com/kelseyhightower/envconfig"
 )
 
+// DBConfig required to configure the database.
 type DBConfig struct {
 	Username string `envconfig:"USERNAME"`
 	Password string `envconfig:"PASSWORD"`
@@ -14,6 +15,7 @@ type DBConfig struct {
 	SSLMode  string `envconfig:"SSL_MODE"`
 }
 
+// AWSConfig required to configure the AWS S3 bucket.
 type AWSConfig struct {
 	Region          string `envconfig:"REGION"`
 	AccessKeyID     string `envconfig:"ACCESS_KEY_ID"`
@@ -21,11 +23,13 @@ type AWSConfig struct {
 	BucketName      string `envconfig:"BUCKET_NAME"`
 }
 
+// JWTConfig required for configuring work with JWT.
 type JWTConfig struct {
 	PublicKeyPath  string `envconfig:"PUBLIC_KEY_PATH"`
 	PrivateKeyPath string `envconfig:"PRIVATE_KEY_PATH"`
 }
 
+// RabbitMQConfig required to configure the RabbitMQ.
 type RabbitMQConfig struct {
 	QueueName         string `envconfig:"QUEUE_NAME"`
 	AMQPConnectionURL string `envconfig:"AMQP_CONNECTION_URL"`
