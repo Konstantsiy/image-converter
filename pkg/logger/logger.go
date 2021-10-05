e983065d4091921c0a11b869cdb848ce17beb397
idi_obnimu <constintinforgit@gmail.com>
2021-10-05 13:02:42 +0300

    Update logger: remove unnecessary methods

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -17,7 +17,7 @@ const (
 	DefaultTimestampFormat     = "2006-01-02 15:04:05"
 )
 
-var ctxLogger = &log.Logger{
+var defaultLogger = &log.Logger{
 	Out: os.Stdout,
 	Formatter: &log.TextFormatter{
 		DisableColors:   false,
@@ -27,35 +27,25 @@ var ctxLogger = &log.Logger{
 	Level: log.InfoLevel,
 }
 
-// ContextWithLogger adds a ctxLogger to the given context.
+// ContextWithLogger adds a defaultLogger to the given context.
 func ContextWithLogger(ctx context.Context) context.Context {
-	return context.WithValue(ctx, loggerKey, ctxLogger)
+	return context.WithValue(ctx, loggerKey, defaultLogger)
 }
 
-// LoggerFromContext returns the logger from the given context.
-func LoggerFromContext(ctx context.Context) *log.Logger {
+// FromContext returns the logger from the given context.
+func FromContext(ctx context.Context) *log.Logger {
 	if logger, ok := ctx.Value(loggerKey).(*log.Logger); ok && logger != nil {
 		return logger
 	}
-	return ctxLogger
+	return defaultLogger
 }
 
 // CompleteRequest logs http requests details like URI, method, status code and duration.
 func CompleteRequest(ctx context.Context, r *http.Request, duration time.Duration, statusCode int) {
-	LoggerFromContext(ctx).WithFields(log.Fields{
+	FromContext(ctx).WithFields(log.Fields{
 		"uri":      r.RequestURI,
 		"method":   r.Method,
 		"duration": duration,
 		"status":   statusCode,
 	}).Info("request completed")
-}
-
-// Info logs message at Info level.
-func Info(ctx context.Context, msg string) {
-	LoggerFromContext(ctx).Infoln(msg)
-}
-
-// Error logs errors at Error level.
-func Error(ctx context.Context, err error) {
-	LoggerFromContext(ctx).Errorln(err)
 }
